fc21477b2ac12b570c8428808f334d467a617e86
Somelauw <Somelauw>
2017-07-24 16:53:19 +0200

    Update example config with new ideas

--- doc/example_config.el
+++ doc/example_config.el
@@ -3,37 +3,58 @@
 (require 'leader)
 (require 'hydra)
 
+(setf evil-org-key-theme '(navigation insert textobjects additional))
+(setf org-special-ctrl-a/e t)
+
 (add-hook 'org-mode-hook
  (lambda ()
-   ;; Set key theme(s)
    (evil-org-mode)
-   (evil-org-set-key-theme '(navigation insert textobjects additional))
 
    ;; Custom mappings
    (evil-define-key 'normal evil-org-mode-map
      (kbd "-") 'org-ctrl-c-minus
      (kbd "|") 'org-table-goto-column
-     (kbd "M-o") 'evil-org-insert-subheading
-     (kbd "M-t") 'evil-org-insert-subtodo)
+     (kbd "M-o") (lambda (arg)
+                   (interactive "P")
+                   (evil-org-eol-call #'org-insert-heading arg))
+     (kbd "M-t") (lambda (arg)
+                   (interactive "P")
+                   (evil-org-eol-call #'org-insert-todo-heading arg)))
 
    ;; Configure leader key
    (evil-leader/set-key-for-mode 'org-mode
+     "." 'hydra-org-state/body
      "t" 'org-todo
      "T" 'org-show-todo-tree
+     "v" 'org-mark-element
      "a" 'org-agenda
      "c" 'org-archive-subtree
      "l" 'evil-org-open-links
      "C" 'org-resolve-clocks)
 
-   ;; Define a hydra
+   ;; Define a transient state for quick navigation
    (defhydra hydra-org-state ()
+     ;; basic navigation
+     ("i" org-cycle)
+     ("I" org-shifttab)
      ("h" org-up-element)
      ("l" org-down-element)
      ("j" org-forward-element)
      ("k" org-backward-element)
+     ;; navigating links
+     ("n" org-next-link)
+     ("p" org-previous-link)
+     ("o" org-open-at-point)
+     ;; navigation blocks
+     ("N" org-next-block)
+     ("P" org-previous-block)
+     ;; updates
+     ("." org-ctrl-c-ctrl-c)
+     ("*" org-ctrl-c-star)
+     ("-" org-ctrl-c-minus)
+     ;; change todo state
      ("H" org-shiftleft)
      ("L" org-shiftright)
-     ("K" org-shiftup)
      ("J" org-shiftdown)
-     ("." org-ctrl-c-ctrl-c)
-     ("i" org-cycle))))
+     ("K" org-shiftup)
+     ("t" org-todo))))
